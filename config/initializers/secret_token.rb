b0249e4b7687c803aabb254c543c6d297eaa5e1c
Paul Thormahlen <pthormahlen@blurb.com>
2011-03-07 20:15:18 -0800

    new zappos app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zapauls::Application.config.secret_token = '0a8f63f0a89a71a6bb6b33722db4ffb6ad02a8045535420002fae295dd3bf595b5f6e3723ef8fca48bfcc9323052385ab95c5cb5e0e0f5a7d6413f74b284326c'
